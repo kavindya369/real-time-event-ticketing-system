8e4db1e2d8a4a4eafce75c9efc669171b4a60679
unknown <you@example.com>
2024-11-16 16:38:28 +0530

    Initial commit for ticketing-backend module

---
+++ ticketing-backend/src/main/java/com/eventticketing/backend/ticketingbackend/TicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eventticketing.backend.ticketingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingBackendApplication.class, args);
+    }
+
+}
